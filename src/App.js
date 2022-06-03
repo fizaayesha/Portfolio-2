98468dd9c38bbc039d0cd1abd346d8c16e5bcf0e
fizaayesha <fizaayesha696@gmail.com>
2022-06-03 15:02:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Sidebar from "./Components/Sidebar";
+import styled from "styled-components";
+import Home from "./Pages/Home";
+import Resume from "./Pages/Resume";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Project from "./Pages/Project";
+import Blogs from "./Pages/Blogs";
+import { Route, Routes as Switching } from "react-router-dom";
+import { BrightnessMediumSharp } from "@material-ui/icons";
+import Switch from "@material-ui/core/Switch";
+import { useState, useEffect } from "react";
+import { MenuRounded } from "@material-ui/icons";
+import { IconButton } from "@material-ui/core";
+import WomeninTech from "./Components/WomeninTech";
+import CssEffects from "./Components/CssEffects";
 function App() {
+  const [theme, setTheme] = useState("dark-theme");
+  const [checked, setChecked] = useState(false);
+  const [navToggle, setNavToggle] = useState(false);
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
+  const themeToggler = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+      setChecked(false);
+    } else {
+      setTheme("light-theme");
+      setChecked(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div div className="App">
+      <Sidebar navToggle={navToggle} />
+      <div className="theme">
+        <div className="mode">
+          <div className="left-content">
+            <BrightnessMediumSharp />
+          </div>
+          <div className="right-content">
+            <Switch
+              value=""
+              checked={checked}
+              inputProps={{ "aria-label": "" }}
+              size="medium"
+              onChange={themeToggler}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="ham-burger-menu">
+        <IconButton onClick={() => setNavToggle(!navToggle)}>
+          <MenuRounded />
+        </IconButton>
+      </div>
+      <MainContentStyles>
+        <Switching>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/projects" element={<Project />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/women-in-tech" element={<WomeninTech />} />
+          <Route path="/css-effects" element={<CssEffects />} />
+        </Switching>
+      </MainContentStyles>
     </div>
   );
 }
-
+const MainContentStyles = styled.main`
+  position: relative;
+  margin-left: 16.3rem;
+  min-height: 100vh;
+  @media screen and (max-width: 1200px) {
+    margin-left: 0;
+  }
+`;
 export default App;
---
+++ src/data/projects.js
@@ -0,0 +1,24 @@
+import faceRecognition from "../Assets/FaceRecognition.jfif";
+import Banking from '../Assets/Online Bnaking.avif'
+
+const projects = [
+  {
+    id: 1,
+    category: "Backend",
+    image: faceRecognition,
+    code: "https://github.com/fizaayesha/face-recognition",
+    title: "Face Recognition",
+    text: "Face Recognition for Secure Transactions",
+  },
+  {
+    id: 2,
+    category: "Backend",
+    image: Banking,
+    code: "https://github.com/fizaayesha/gripbank-frontend",
+    visit: "https://grip-banking-webapp.netlify.app/",
+    title: "Online Banking System",
+    text: "Showcase of online banking system",
+  },
+];
+
+export default projects;
